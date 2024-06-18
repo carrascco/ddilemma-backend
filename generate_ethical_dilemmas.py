91f9ceac0dcd17aedd59d6d0591804398cdccecb
Miguel Carrasco <quintes32-miguel@hotmail.es>
2024-06-19 00:02:03 +0200

    Pre - Defensa

--- generate_ethical_dilemmas.py
+++ generate_ethical_dilemmas.py
@@ -71,30 +71,7 @@ def generate_possible_responses(dilemma):
     dilemma_response = chat_completion.choices[0].message.content
     return dilemma_response
 
-
-#---------------------------------------------------------------------------
-#-----------Conexión a BBDD para extraer noticia y generar dilema-----------
-#---------------------------------------------------------------------------
-# TABLE noticias (
-#     id serial PRIMARY KEY,
-#     url text,
-#     titulo text NOT NULL,
-#     cuerpo text NOT NULL,
-#     fuente text,
-#     url_imagen text
-# );
-
-# # Se obtiene la URL de la base de datos de las variables de entorno
-# DATABASE_URL = os.environ['DATABASE_URL']
-# # Conexión a la base de datos
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-# # Crea un cursor para ejecutar consultas SQL
-# cur = conn.cursor()
-
-# # Seleccionar la última noticia (basandose en el ID que es autoincremental)
-# cur.execute("SELECT * FROM noticias ORDER BY id DESC LIMIT 1;")
-# noticia = cur.fetchone()
+#------------------------CONEXIÓN A LA BASE DE DATOS------------------------
 
 import firebase_admin
 from firebase_admin import credentials
@@ -126,15 +103,6 @@ print()
 respuestas = generate_possible_responses(dilema)
 
 
-#------------------------Inserción del dilema en la BBDD------------------------
-#      Columna      |            Tipo             | Ordenamiento | Nulable  |             Por omisión
-# ------------------+-----------------------------+--------------+----------+-------------------------------------
-#  id               | integer                     |              | not null | nextval('dilemas_id_seq'::regclass)
-#  contenido        | text                        |              | not null |
-#  respuestas       | text[]                      |              | not null |
-#  fecha_generacion | timestamp without time zone |              |          | CURRENT_TIMESTAMP
-#  id_noticia       | integer                     |              |          |
-# Esto es la tabla de dilemas
 
 # Extraer las posibles respuestas
 respuestas = re.split(r'\d+\. ', respuestas)
@@ -171,39 +139,3 @@ try:
 except Exception as e:
     print("Error al insertar datos en Firebase:", e)
 
-
-# # Preparar las respuestas para ser insertadas en la base de datos
-# insert_query = f"INSERT INTO dilemas (contenido, respuestas, id_noticia) VALUES (%s, %s, %s);"
-
-# # Ejecuta la consulta SQL para insertar el dilema en la base de datos
-# cur.execute(insert_query, (dilema, respuestas, noticia[0]))
-# conn.commit()
-
-# #Comprobamos que se ha insertado el dilema      
-# cur.execute("SELECT * FROM dilemas ORDER BY id DESC LIMIT 1;")
-# print("\nDilema introducido correctamente: ", cur.fetchall())
-
-# # Cierra el cursor y la conexión
-# cur.close()
-# conn.close()
-
-
-# #------------------------RESETEO DE BBDD de VOTOS------------------------
-# FIREBASE_API_URL=os.environ['FIREBASE_API_URL']
-
-
-
-# # Se resetean los votos a 0
-# data = {
-#     "votosA": 0,
-#     "votosB": 0,
-#     "votosC": 0,
-#     "votosD": 0
-# }
-
-# response = requests.put(FIREBASE_API_URL, json=data)
-# print(response.json())
-
-
-# #------------------------FIN DE LA EJECUCIÓN------------------------
-# #-------------------------------------------------------------------
